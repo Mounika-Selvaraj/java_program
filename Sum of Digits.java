14224058280d8e13a1cca96c5a118d38bf9805a2
Mounika-Selvaraj <146700795+Mounika-Selvaraj@users.noreply.github.com>
2024-07-21 11:48:21 +0530

    Create Sum of Digits.java

---
+++ Sum of Digits.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class SumOfDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = scanner.nextInt();
+        int sum = 0;
+        while (num != 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        System.out.println("The sum of the digits is: " + sum);
+        scanner.close();
+    }
+}
